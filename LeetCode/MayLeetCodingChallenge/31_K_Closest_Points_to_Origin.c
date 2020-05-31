556d9e7415f8d8ba0f8c6d1cdf7a54928df220fd
Илюха <boy.toy.destroy@gmail.com>
2020-05-31 03:34:27 +0300

    Update 31_K_Closest_Points_to_Origin.c

--- LeetCode/MayLeetCodingChallenge/31_K_Closest_Points_to_Origin.c
+++ LeetCode/MayLeetCodingChallenge/31_K_Closest_Points_to_Origin.c
@@ -6,36 +6,23 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 
-typedef struct point{
-    int key;
-    int x;
-    int y;
-}_arr;
 int cmp(const void *a, const void *b) {
-    _arr* A = (_arr*)a;
-    _arr* B = (_arr*)b;
-    return A->key - B->key;
+    int* A = *(const int**)a;
+    int* B = *(const int**)b;
+    return (A[0]*A[0] + A[1]*A[1]) - (B[0]*B[0] + B[1]*B[1]);
 }
 int** kClosest(int** points, int pointsSize, int* pointsColSize, int K, int* returnSize, int** returnColumnSizes){
     *returnColumnSizes = (int*) malloc(sizeof(int)*(*returnSize=K));
     int** ans = (int**) malloc(sizeof(int*)*(*returnSize));
-    _arr* dist = (_arr*)malloc(sizeof(struct point)*pointsSize);
-    for(int i=0;i<*returnSize;++i){
+    for(int i=0;i<*returnSize;++i)
         ans[i] = (int*) malloc(sizeof(int)*((*returnColumnSizes)[i]=*pointsColSize));
-    }
-    
-    for(int i=0;i<pointsSize;++i){
-        dist[i].key = points[i][0]*points[i][0]+points[i][1]*points[i][1];
-        dist[i].x = points[i][0];
-        dist[i].y = points[i][1];
-    }
-    
-    qsort(dist,pointsSize,sizeof(struct point),cmp);
+        
+    qsort(points,pointsSize,sizeof(points),cmp);
 
     for(int i=K-1;i>=0;--i){
-        ans[i][0]=dist[i].x;
-        ans[i][1]=dist[i].y;
+        ans[i][0]=points[i][0];
+        ans[i][1]=points[i][1];
     }
-    //free(dist);
+    
     return ans;
 }
