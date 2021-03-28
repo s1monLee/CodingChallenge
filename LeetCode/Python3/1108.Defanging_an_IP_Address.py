fe51f6c465c2ff550d65a819672654c053d09558
s1monLee <boy.toy.destroy@gmail.com>
2021-03-28 17:17:21 +0300

    Create 1108.Defanging_an_IP_Address.py

---
+++ LeetCode/Python3/1108.Defanging_an_IP_Address.py
@@ -0,0 +1,7 @@
+#Given a valid (IPv4) IP address, return a defanged version of that IP address.
+#A defanged IP address replaces every period "." with "[.]".
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        import re
+        return re.sub(r'\D',r'[.]',address)
