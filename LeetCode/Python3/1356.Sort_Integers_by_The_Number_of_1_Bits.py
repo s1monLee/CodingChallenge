b29e58767a265f3ed23e1291614eef9227a7aa19
s1monLee <boy.toy.destroy@gmail.com>
2021-04-03 21:39:59 +0300

    Update 1356.Sort_Integers_by_The_Number_of_1_Bits.py

--- LeetCode/Python3/1356.Sort_Integers_by_The_Number_of_1_Bits.py
+++ LeetCode/Python3/1356.Sort_Integers_by_The_Number_of_1_Bits.py
@@ -1,2 +1,7 @@
 def sortByBits(self, A):
         return sorted(A, key=lambda a: [bin(a).count('1'), a])
+"""
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key = lambda x : (sum((x>>i)&1 for i in range(32)), x))
+"""
