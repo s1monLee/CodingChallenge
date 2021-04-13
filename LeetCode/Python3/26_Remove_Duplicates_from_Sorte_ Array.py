1d76a1ceb4fd84fe043836c793795cc09c5ac2fb
s1monLee <boy.toy.destroy@gmail.com>
2021-04-14 00:00:41 +0300

    Create 26_Remove_Duplicates_from_Sorte_ Array.py

---
+++ LeetCode/Python3/26_Remove_Duplicates_from_Sorte_ Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if nums == []: return 0
+        last = nums[0]
+        ind = []
+        ans = 1
+        for i in range(1, len(nums)):
+            if(nums[i] == last):
+                ind.append(i)
+            else:
+                ans += 1
+                last = nums[i]
+        ind = ind[::-1]
+        for i in range(len(ind)):
+            nums.pop(ind[i])
+            
+        return ans
