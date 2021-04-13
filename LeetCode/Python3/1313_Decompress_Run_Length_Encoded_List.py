e4a03e036a69bf6be36627895da0e4554b57fdd5
s1monLee <boy.toy.destroy@gmail.com>
2021-04-13 23:43:03 +0300

    Update 1313_Decompress_Run_Length_Encoded_List.py

--- LeetCode/Python3/1313_Decompress_Run_Length_Encoded_List.py
+++ LeetCode/Python3/1313_Decompress_Run_Length_Encoded_List.py
@@ -4,3 +4,5 @@ class Solution:
         for i, num in enumerate(nums):
             if i%2 == 0: ans += [nums[i+1]]*num
         return ans
+        #one-line
+        #return sum([nums[i]*[nums[i+1]] for i in range(0,len(nums),2)],[])
