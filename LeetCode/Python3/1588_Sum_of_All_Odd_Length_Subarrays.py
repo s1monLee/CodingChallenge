64cf83b56efe2a1c7977389ad73480c34e1be6b8
s1monLee <boy.toy.destroy@gmail.com>
2021-05-14 00:17:38 +0300

    Update 1588_Sum_of_All_Odd_Length_Subarrays.py

--- LeetCode/Python3/1588_Sum_of_All_Odd_Length_Subarrays.py
+++ LeetCode/Python3/1588_Sum_of_All_Odd_Length_Subarrays.py
@@ -1,9 +1,17 @@
+"""
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # corner case
-        
         res = 0; freq = 0; n = len(arr)
         for i in range(n):
             freq = freq-(i+1)//2+(n-i+1)//2
             res += freq*arr[i]
         return res
+"""
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        ans = 0
+        for i in range(len(arr)):
+            for j in range(i, len(arr),2):
+                ans += sum(arr[i:j+1])
+        return ans
+                
