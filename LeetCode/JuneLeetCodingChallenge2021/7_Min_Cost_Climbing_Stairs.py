6fa5b3f08fbaffeaa22f8b1d5c546c33cbd6c0f2
s1monLee <boy.toy.destroy@gmail.com>
2021-06-07 21:09:38 +0300

    Update 7_Min_Cost_Climbing_Stairs.py

--- LeetCode/JuneLeetCodingChallenge2021/7_Min_Cost_Climbing_Stairs.py
+++ LeetCode/JuneLeetCodingChallenge2021/7_Min_Cost_Climbing_Stairs.py
@@ -7,3 +7,13 @@ class Solution:
             dp0 = dp1
             dp1 = tmp
         return min(dp0, dp1)
+    """
+    class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        return min(dp[-1], dp[-2])
+    """
