f81b7c6c40128e8da2faaeaa19a1ebee5c99f301
s1monLee <boy.toy.destroy@gmail.com>
2021-06-07 21:01:36 +0300

    Create 7_Min_Cost_Climbing_Stairs.py

---
+++ LeetCode/JuneLeetCodingChallenge2021/7_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp0 = cost[0]
+        dp1 = cost[1]
+        for i in range(2, len(cost)):
+            tmp = cost[i] + min(dp0, dp1)
+            dp0 = dp1
+            dp1 = tmp
+            print(tmp, dp0, dp1)
+        return min(dp0, dp1)
