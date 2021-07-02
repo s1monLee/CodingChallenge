b3dcc8b13c6a3c58a71524d75c349b130c7b5d69
s1monLee <boy.toy.destroy@gmail.com>
2021-07-02 23:28:47 +0300

    Create 242_Valid_Anagram.py

---
+++ LeetCode/Python3/242_Valid_Anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = set(s)
+        if len(s)!=len(t):
+            return False
+        for i in a:
+            if s.count(i)!=t.count(i):
+                return False
+        return True
